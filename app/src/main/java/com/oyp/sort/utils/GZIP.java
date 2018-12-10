59e5108a4290181c7819ecafb77a64a4d3cf89ab
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2018-12-10 21:03:47 +0800

    1、使用GZIP压缩json文件

---
+++ app/src/main/java/com/oyp/sort/utils/GZIP.java
@@ -0,0 +1,62 @@
+package com.oyp.sort.utils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+ 
+/**
+ * Gzip 压缩字符串
+ */
+public class GZIP {
+    /**
+     * 字符串的压缩
+     *
+     * @param str 待压缩的字符串
+     * @return 返回压缩后的字符串
+     * @throws IOException
+     */
+    public static String compress(String str) throws IOException {
+        if (null == str || str.length() <= 0) {
+            return str;
+        }
+        // 创建一个新的输出流
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        // 使用默认缓冲区大小创建新的输出流
+        GZIPOutputStream gzip = new GZIPOutputStream(out);
+        // 将字节写入此输出流
+        gzip.write(str.getBytes("utf-8"));  //因为后台默认字符集有可能是GBK字符集，所以此处需指定一个字符集
+        gzip.close();
+        // 使用指定的 charsetName，通过解码字节将缓冲区内容转换为字符串
+        return out.toString("ISO-8859-1");
+    }
+
+    /**
+     * 字符串的解压
+     *
+     * @param str 对字符串解压
+     * @return 返回解压缩后的字符串
+     * @throws IOException
+     */
+    public static String unCompress(String str) throws IOException {
+        if (null == str || str.length() <= 0) {
+            return str;
+        }
+        // 创建一个新的输出流
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        // 创建一个 ByteArrayInputStream，使用 buf 作为其缓冲区数组
+        ByteArrayInputStream in = new ByteArrayInputStream(str.getBytes("ISO-8859-1"));
+        // 使用默认缓冲区大小创建新的输入流
+        GZIPInputStream gzip = new GZIPInputStream(in);
+        byte[] buffer = new byte[256];
+        int n = 0;
+
+        // 将未压缩数据读入字节数组
+        while ((n = gzip.read(buffer)) >= 0) {
+            out.write(buffer, 0, n);
+        }
+        // 使用指定的 charsetName，通过解码字节将缓冲区内容转换为字符串
+        return out.toString("utf-8");
+    }
+}
--- app/src/main/java/com/oyp/sort/utils/stroke/utils/StrokeUtils.java
+++ app/src/main/java/com/oyp/sort/utils/stroke/utils/StrokeUtils.java
@@ -9,12 +9,14 @@ import android.os.Environment;
 
 import android.util.Log;
 
+import com.oyp.sort.utils.GZIP;
 import com.oyp.sort.utils.JSONUtil;
 import com.oyp.sort.utils.LocalFileUtils;
 import com.oyp.sort.utils.stroke.bean.Stroke;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.HashMap;
@@ -30,12 +32,41 @@ public class StrokeUtils {
 
     private static HashMap<String, Stroke> mapper;
 
-    public static StrokeUtils newInstance(Context context) {
+    public static StrokeUtils newInstance(Context context)  {
         if (mapper == null) {
             String strokeJson = LocalFileUtils.getStringFormAsset(context, "stroke.json");
+            try {
+                String gzipStrokeJson = GZIP.compress(strokeJson);
+                writeFile(gzipStrokeJson, "gzipStroke.json");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             mapper = JSONUtil.toCollection(strokeJson, HashMap.class, String.class, Stroke.class);
         }
         return factory;
+    }
+
+    private static void writeFile(String mapperJson, String fileName) {
+        try {
+            File file = new File(Environment.getExternalStorageDirectory(), fileName);
+            Log.d(TAG, "file.exists():" + file.exists() + " file.getAbsolutePath():" + file.getAbsolutePath());
+            // 如果父目录不存在，创建父目录
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+            // 如果已存在,删除旧文件
+            if (file.exists()) {
+                file.delete();
+            }
+            file.createNewFile();
+            // 将格式化后的字符串写入文件
+            Writer write = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
+            write.write(mapperJson);
+            write.flush();
+            write.close();
+        } catch (Exception e) {
+            Log.e(TAG, "ios = " + Log.getStackTraceString(e));
+        }
     }
 
     /**
