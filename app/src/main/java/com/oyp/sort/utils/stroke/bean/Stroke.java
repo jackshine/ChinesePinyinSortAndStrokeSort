845ac1735547687dd8d4d6c0315e94d5003e4314
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2018-12-10 20:19:13 +0800

    1、修改json的key来压缩json的体积

--- app/src/main/java/com/oyp/sort/utils/stroke/bean/Stroke.java
+++ app/src/main/java/com/oyp/sort/utils/stroke/bean/Stroke.java
@@ -1,21 +1,27 @@
 package com.oyp.sort.utils.stroke.bean;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Stroke {
     /**
      * 序号
      */
+    @JsonProperty("o")
     private String order;
     /**
      * 汉字
      */
+    @JsonProperty("n")
     private String name;
     /**
      * 编码
      */
+    @JsonProperty("c")
     private String code;
     /**
      * 笔画数量
      */
+    @JsonProperty("s")
     private String strokeSum;
 
     public String getOrder() {
